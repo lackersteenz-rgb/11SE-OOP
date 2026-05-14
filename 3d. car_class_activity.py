a02dad26bfeaed16a1302874c7b284dcccab322c
Zoe Lackersteen <lackersteenz@gmail.com>
2026-05-14 02:12:38 +0000

    8th

--- 3d. car_class_activity.py
+++ 3d. car_class_activity.py
@@ -9,14 +9,21 @@ class Car:
         self.model = model
         self.year = year
         self.price = price
+        self.for_sale = False
 
     def __str__(self):
-
+        if self.for_sale == True:
+            return '|Make: '+self.make+' | Model: '+self.model +'| FOR SALE |'
+        else:
+            return '|Make: '+self.make+' | Model '+self.model +'| NOT FOR SALE |'
 
 c1 = Car('Mazda','6',2005)
+c2 = Car('Tesla','S',2017)
+c2.for_sale = True
+c3 = Car('Porche','911',2020)
+c4 = Car('Golf','TDI',2016)
 
-
-cars = [c1]
+cars = [c1,c2,c3,c4]
 
 for car in cars:
     print(car)
