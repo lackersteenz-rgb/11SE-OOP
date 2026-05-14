f885af81a680ad35b3bd2808c1db404c1dd266ea
Zoe Lackersteen <lackersteenz@gmail.com>
2026-05-14 02:24:19 +0000

    9th

--- 4a. OOP_methods_activities.py
+++ 4a. OOP_methods_activities.py
@@ -13,6 +13,44 @@ class Pet:
     
     def have_birthday(self):
         self.age += 1
+
+    def vaccinated(self):
+        if self.vaccinated == True:
+            print(self.name,'is already vaccinated')
+        else:
+            self.vaccinated = True
+        
+    def clear_balance(self):
+        self.account_balance = 0
+
+    def calculate_human_age(self):
+        if self.category == 'Dog':
+            print(self.name,'human age:',self.age*7)
+        elif self.category == 'Cat':
+            print(self.name,'human age:',self.age*6)
+        else:
+            print(self.name,'human age is unknown')
+
+    def __str__(self):
+        payment_status = 'unregistered'
+        if len(self.ccard) == 19:
+            payment_status = 'registered'
+        
+    my_status = 'Name: ' + self.name +'\nCategory: ' + self.category + '\nAge: ' + 
+str(self.age) +'\nPayment status: ' + payment_status + '\nVaccinated: '+ 
+str(self.vaccinated)
+        return my_status
+#It is correct, exactly. But still reports an error.
+
+p1 = Pet(name = 'Bonnie', category = 'Cat', age = 10)
+p1.have_birthday()
+p1.vaccinate()
+p1.vaccinate()
+
+print(p1)
+p1.calculate_human_age()
+    
+
     
     
 
