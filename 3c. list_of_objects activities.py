177c0ecb247a7a635ad7d8ba03748bbbbe7f4162
Zoe Lackersteen <lackersteenz@gmail.com>
2026-05-14 02:08:32 +0000

    7th

--- 3c. list_of_objects activities.py
+++ 3c. list_of_objects activities.py
@@ -1,6 +1,6 @@
 # Learning intentions:
 # - Create a list of pets
-# - Use a for loop to print out various information about pets
+# - Use a for loop to print out various information about pets\
 
 class Pet:
     def __init__(self, name, category, age = 0):
@@ -9,6 +9,28 @@ class Pet:
         self.age = age
         self.ccard = 'unknown'
         self.vaccinated = False
+
+    def __str__(self):
+        payment_status = 'unregistered'
+        if len(self.ccard) == 19:
+            payment_status = 'registered'
+
+        my_status = 'Name: ' + self.name +'\nCategory: ' + self.category + '\nAge: ' + str(self.age) +'\nPayment status: ' + payment_status + '\nVaccinated: '+ str(self.vaccinated)
+        return my_status
+
+p1 = Pet('Bonnie', 'Cat')
+p2 = Pet('Clyde', 'Dog',7)
+p3 = Pet(category = 'Rabbit', name = 'Ruby', age = 13)
+p4 = Pet('George','Cat',6)
+p5 = Pet('Sally','Hampster',2)
+
+pets = [p1, p2, p3, p4]
+pets.append(p5)
+
+for pet in pets:
+    pet.vaccinated = True
+    print(pet)
+    print('')
 
 
 
