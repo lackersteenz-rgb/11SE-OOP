3d0a377d48ac1909c301922040bbb9ed38b7e1f9
Zoe Lackersteen <lackersteenz@gmail.com>
2026-05-14 01:57:16 +0000

    5th

--- 3a. pet_class_activities.py
+++ 3a. pet_class_activities.py
@@ -2,7 +2,27 @@
 # - Create a class pet with same information as in previous examples
 # - Create an object instance of class pet
 
+class Pet:
+    def __init__(self, name, category, age):
+        self.name = name
+        self.category = category
+        self.age = age
+        self.vaccinated = False
+        self.ccard = 'unknown'
+        self.billing_address = 'unknown'
+        self.owner_name = 'unknown'
+        self.account_balance = 0
 
+p1 = Pet('Bonnie', 'Cat', 3)
+p2 = Pet('Foxy', 'Dog', 5)
+
+print(p2.name)
+print(p2.category)
+print(p2.vaccinated)
+
+p2.owner_name = 'John'
+
+print(p2.owner_name)
 
 
 #ACTIVITIES:
