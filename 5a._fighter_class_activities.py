a6646ef4d0c9591a3e561750b3b195870ab11bb4
Zoe Lackersteen <lackersteenz@gmail.com>
2026-05-18 01:02:44 +0000

    13th

--- 5a._fighter_class_activities.py
+++ 5a._fighter_class_activities.py
@@ -3,6 +3,56 @@
 #2. Test the game 
 #3. Implement the game with a private __health attribute
 
-import random
+import random, time
 
 class Fighter:
+    def __init__(self,name, starting_health, weapon, shield):
+        self.name = name
+        self.health = starting_health
+        self.weapon = weapon
+        self.shield = shield
+
+    def report(self):
+        print(self.name+':'+ 'Health: '+ str(self.__health))
+
+    def is_dead(self):
+        if self.__health <= 0:
+            return True
+        else:
+            return False
+
+    def random_attack(self):
+        attack_power = random.randint(self.weapon/2,self.weapon*2)
+        print('Attack power:', attack_power)
+        return attack_power
+    
+    def defend(self,attack_power):
+        damage = attack_power - self.shield
+        if damage > 0:
+            self.__health -= damage
+            print('Damage:', damage)
+        else:
+            print('No damage')
+
+you = Fighter('You',100,60,20)
+troll = Fighter('Troll',200,30,10)
+
+you.report()
+troll.report()
+
+while True:
+    print('You attack the troll')
+    troll.defend(you.random_attack())
+    troll.report()
+    time.sleep(2)
+    if troll.is_dead():
+        print('You win')
+        break
+    print('The troll attacks you . . .')
+    you.defent(troll.random_attack())
+    you.report()
+    time.sleep(2)
+    if you.is_dead():
+        print('The troll wins')
+        break
+    print('')
