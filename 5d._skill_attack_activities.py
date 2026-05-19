c283726d82b87171cf377e4de137688d4f5f918a
Zoe Lackersteen <lackersteenz@gmail.com>
2026-05-19 05:12:36 +0000

    15th

--- 5d._skill_attack_activities.py
+++ 5d._skill_attack_activities.py
@@ -22,9 +22,28 @@ class Fighter:
             return False
 
     def random_attack(self):
-        attack_power = random.randint(self.weapon/2, self.weapon*2)
+        attack_power = random.randint(self.weapon//2, self.weapon*2)
         print('Attack power:', attack_power)
         return attack_power
+
+   
+    def skill_attack(self):
+        attack_power = random.randint(self.weapon//2, self.weapon*2)
+        target = random.randint(2,6)
+        print('Hit enter in exactly',target,'seconds')
+        tic = time.time()
+        input()
+        toc = time.time()
+        time_taken = toc - tic
+        multiplier = 3 - abs(3-time_taken)
+        if multiplier < 2:
+            multiplier = 0
+
+
+        print('Attack power:', attack_power)
+        print('Multiplier:', multiplier)
+        return attack_power*multiplier
+
 
     def defend(self,attack_power):
         damage = attack_power - self.shield
@@ -40,4 +59,21 @@ troll = Fighter('Troll',300,30,10)
 
 you.report()
 troll.report()
+
+while True:
+    print('You attack the troll')
+    troll.defend(you.skill_attack())
+    troll.report()
+    time.sleep(2)
+    if troll.is_dead():
+        print('You win')
+        break
+    print('The troll attacks you . . .')
+    you.defend(troll.random_attack())
+    you.report()
+    time.sleep(2)
+    if you.is_dead():
+        print('The troll wins')
+        break
+    print('')
 
